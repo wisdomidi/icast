3e7d8286de12aa31cfcf2d25a3c9e9fe64aa3257
Wisdom Idi <oweezyidi@gmail.com>
2020-04-22 22:25:37 +0100

    .

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 
 
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
