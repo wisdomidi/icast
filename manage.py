5a6409512fca65f8ab3e6f22ad5cace5ffc9e5c9
Wisdom Idi <oweezyidi@gmail.com>
2020-04-22 03:56:41 +0100

    .

--- manage.py
+++ manage.py
@@ -2,10 +2,8 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask import Flask
 
-from app import app
+from flask import app
 from models import Actor, Movie, db
-
-app = Flask(__name__)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
