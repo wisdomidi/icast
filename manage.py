913423fe8af527101afa02c298a6b7e6ecfc4a6d
Wisdom Idi <oweezyidi@gmail.com>
2020-04-22 23:33:38 +0100

    .

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask import Flask
 
-#from app import app
+from app import app
 from models import db, Actor, Movie
 
 #print(app)
